ff4c3c2da29433cb54feac8278dab974686bdeff
rayyanrafan7 <rayyan.rafan@gmail.com>
2026-05-01 09:34:40 -0300

    Fix SNS topic variables in module 02

--- itmo-463-563/module-02/main.tf
+++ itmo-463-563/module-02/main.tf
@@ -50,10 +50,10 @@ output "raw_url" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic
 ##############################################################################
 resource "aws_sns_topic" "user_updates" {
-  name = 
+  name = var.sns-topic
 
   tags = {
-    Name        = 
+    Name        = var.tag-name
     Environment = "project"
   }
 }
