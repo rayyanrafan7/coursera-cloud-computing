3b3fbc12d6184434631de7949fecfa43cf0b57f2
rayyanrafan7 <rayyan.rafan@gmail.com>
2026-05-01 12:40:57 -0300

    Propagate Name tag to ASG instances

--- itmo-463-563/module-02/main.tf
+++ itmo-463-563/module-02/main.tf
@@ -165,7 +165,12 @@ resource "aws_autoscaling_group" "asg" {
     id = aws_launch_template.lt.id
     version = "$Latest"
   }  
-  
+  tag {
+    key                 = "Name"
+    value               = var.tag-name
+    propagate_at_launch = true
+  }
+
 }
 
 ##############################################################################
