fbda4be320cdfadc4483f80f8e7317aebbdd7b51
rayyanrafan7 <rayyan.rafan@gmail.com>
2026-05-02 07:54:30 -0300

    Complete module 5 create and destroy scripts

--- itmo-444-544/module-05/create-env.sh
+++ itmo-444-544/module-05/create-env.sh
@@ -1,154 +1,161 @@
 #!/bin/bash
 ##############################################################################
-# Module-05
-# This assignment requires you to modify your previous scripts and use the 
-# Launch Template and Autoscaling group commands for creating EC2 instances
-# You will need an additional script to generate a JSON file with parameters
-# for your launch template. You will need to add an extras storage harddisk (EBS)
-# to each EC2 instance, define and IAM profile to use and define the name of two
-# S3 buckets to create
-# 
-# You will need to define these variables in a txt file named: arguments.txt
-# 1 image-id
-# 2 instance-type
-# 3 key-name
-# 4 security-group-ids
-# 5 count
-# 6 user-data file name
-# 7 Tag (use the module name - later we can use the tags to query/filter
-# 8 Target Group (use your initials)
-# 9 elb-name (use your initials)
-# 10 Availability Zone 1
-# 11 Availablitty Zone 2
-# 12 Launch Template Name
-# 13 ASG name
-# 14 ASG min
-# 15 ASG max
-# 16 ASG desired
-# 17 AWS Region for LaunchTemplate (use your default region)
-# 18 EBS disk storage size in GB
-# 19 S3 Bucket One
-# 20 S3 Bucket Two
+# Module-05 Create Script
+# Creates launch template, ASG, target group, load balancer, EBS mapping, and S3.
 ##############################################################################
 
 ltconfigfile="./config.json"
 
 if [ $# = 0 ]
 then
-  echo "You don't have enough variables in your arugments.txt, perhaps you forgot to run: bash ./create-env.sh \$(< ~/arguments.txt)"
-  exit 1 
+  echo "You don't have enough variables in your arguments.txt, perhaps you forgot to run: bash ./create-env.sh $(< ~/arguments.txt)"
+  exit 1
 elif ! [[ -a $ltconfigfile ]]
-  then
-  echo "The launch template configuration JSON file doesn't exist - make sure you run/ran the command: bash ./create-lt-json.sh \$(< ~/arguments.txt) command before running the create-env.sh \$(< ~/arguments.txt)"
-   echo "Now exiting the program..."
-   exit 1
-# else run the creation logic
+then
+  echo "The launch template configuration JSON file doesn't exist."
+  echo "Run: bash ./create-lt-json.sh $(< ~/arguments.txt)"
+  exit 1
 else
-if [ -a $ltconfigfile ]
-    then
-    echo "Launch template data file: $ltconfigfile exists..." 
-fi
+
+echo "Launch template data file: $ltconfigfile exists..."
+
 echo "Finding and storing default VPCID value..."
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-vpcs.html
-VPCID=$(aws ec2 describe-vpcs --filters "Name=is-default,Values=true" --query "Vpcs[*].VpcId" --output=text)
+VPCID=$(aws ec2 describe-vpcs \
+  --filters "Name=is-default,Values=true" \
+  --query "Vpcs[0].VpcId" \
+  --output text)
 echo $VPCID
 
-echo "Finding and storing the subnet IDs for defined in arguments.txt Availability Zone 1 and 2..."
-SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
-SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+echo "Finding and storing the subnet IDs for Availability Zone 1 and 2..."
+SUBNET2A=$(aws ec2 describe-subnets \
+  --query 'Subnets[0].SubnetId' \
+  --output text \
+  --filters "Name=availability-zone,Values=${10}" "Name=default-for-az,Values=true")
+
+SUBNET2B=$(aws ec2 describe-subnets \
+  --query 'Subnets[0].SubnetId' \
+  --output text \
+  --filters "Name=availability-zone,Values=${11}" "Name=default-for-az,Values=true")
+
 echo $SUBNET2A
 echo $SUBNET2B
 
 echo "Creating the AutoScalingGroup Launch Template..."
-aws ec2 create-launch-template --launch-template-name ${12} --version-description AutoScalingVersion1 --launch-template-data file://config.json --region ${17}
+aws ec2 create-launch-template \
+  --launch-template-name ${12} \
+  --version-description AutoScalingVersion1 \
+  --launch-template-data file://config.json \
+  --region ${17}
+
 echo "Launch Template created..."
 
-# Launch Template Id
-LAUNCHTEMPLATEID=
-
-echo "Creating the TARGET GROUP and storing the ARN in \$TARGETARN"
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=
+echo "Creating the TARGET GROUP and storing the ARN in TARGETARN..."
+TARGETARN=$(aws elbv2 create-target-group \
+  --name ${8} \
+  --protocol HTTP \
+  --port 80 \
+  --vpc-id $VPCID \
+  --target-type instance \
+  --query 'TargetGroups[0].TargetGroupArn' \
+  --output text)
 echo $TARGETARN
 
 echo "Creating ELBv2 Elastic Load Balancer..."
-#https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-load-balancer.html
-ELBARN=
+ELBARN=$(aws elbv2 create-load-balancer \
+  --name ${9} \
+  --subnets $SUBNET2A $SUBNET2B \
+  --security-groups ${4} \
+  --scheme internet-facing \
+  --type application \
+  --query 'LoadBalancers[0].LoadBalancerArn' \
+  --output text)
 echo $ELBARN
 
-# AWS elbv2 wait for load-balancer available
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
 echo "Waiting for load balancer to be available..."
 aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
 echo "Load balancer available..."
-# create AWS elbv2 listener for HTTP on port 80
-#https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
-aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TARGETARN
 
-echo 'Creating Auto Scaling Group...'
-# Create Autoscaling group ASG - needs to come after Target Group is created
-# Create autoscaling group
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/create-auto-scaling-group.html
-aws autoscaling create-auto-scaling-group 
+echo "Creating listener on port 80..."
+aws elbv2 create-listener \
+  --load-balancer-arn $ELBARN \
+  --protocol HTTP \
+  --port 80 \
+  --default-actions Type=forward,TargetGroupArn=$TARGETARN
 
-echo 'Waiting for Auto Scaling Group to spin up EC2 instances and attach them to the TargetARN...'
-# Create waiter for registering targets
-# https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/target-in-service.html
-aws elbv2 wait target-in-service --target-group-arn 
-echo "Targets attached to Auto Scaling Group..."
+echo "Creating Auto Scaling Group..."
+aws autoscaling create-auto-scaling-group \
+  --auto-scaling-group-name ${13} \
+  --launch-template LaunchTemplateName=${12},Version='$Latest' \
+  --min-size ${14} \
+  --max-size ${15} \
+  --desired-capacity ${16} \
+  --target-group-arns $TARGETARN \
+  --vpc-zone-identifier "$SUBNET2A,$SUBNET2B" \
+  --tags Key=Name,Value=${7},PropagateAtLaunch=true
 
-# Collect Instance IDs
-# https://stackoverflow.com/questions/31744316/aws-cli-filter-or-logic
-INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' --filter "Name=instance-state-name,Values=running,pending")
+echo "Waiting for Auto Scaling Group to create EC2 instances..."
+sleep 120
+
+echo "Collecting Instance IDs..."
+INSTANCEIDS=$(aws ec2 describe-instances \
+  --output text \
+  --query 'Reservations[*].Instances[*].InstanceId' \
+  --filters "Name=instance-state-name,Values=running,pending" "Name=tag:Name,Values=${7}")
+
+echo $INSTANCEIDS
 
 if [ "$INSTANCEIDS" != "" ]
-  then
-    aws ec2 wait instance-running --instance-ids 
-    echo "Finished launching instances..."
-  else
-    echo 'There are no running or pending values in $INSTANCEIDS to wait for...'
-fi 
+then
+  echo "Waiting until instances are in RUNNING state..."
+  aws ec2 wait instance-running --instance-ids $INSTANCEIDS
+  echo "Instances are running."
+else
+  echo "There are no running or pending instances to wait for..."
+fi
 
-# Add S3api commands to create two S3 buckets
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/index.html
+echo "Waiting for targets to become healthy..."
+aws elbv2 wait target-in-service --target-group-arn $TARGETARN
+echo "Targets are healthy."
+
 echo "Creating S3 bucket: ${19}..."
-aws s3api create-bucket 
-echo "Created S3 bucket: ${19}..."
+aws s3api create-bucket \
+  --bucket ${19} \
+  --region ${17} \
+  --create-bucket-configuration LocationConstraint=${17}
 
 echo "Creating S3 bucket: ${20}..."
-aws s3api create-bucket 
-echo "Created S3 bucket: ${20}..."
+aws s3api create-bucket \
+  --bucket ${20} \
+  --region ${17} \
+  --create-bucket-configuration LocationConstraint=${17}
 
-# S3 commands
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html
-# Upload illinoistech.png and rohit.jpg to bucket ${19}
-echo "Uploading image: ./images/illinoistech.png to s3://${19}..."
-aws s3 cp 
-echo "Uploaded image: ./images/illinoistech.png to s3://${19}..."
+echo "Preparing upload files..."
+mkdir -p ./images
 
-echo "Uploading image: ./images/rohit.jpg to s3://${19}..."
-aws s3 cp 
-echo "Uploaded image: ./images/rohit.jpg to s3://${19}..."
+if [ ! -f ./images/illinoistech.png ]; then echo "module5 file 1" > ./images/illinoistech.png; fi
+if [ ! -f ./images/rohit.jpg ]; then echo "module5 file 2" > ./images/rohit.jpg; fi
+if [ ! -f ./images/elevate.webp ]; then echo "module5 file 3" > ./images/elevate.webp; fi
+if [ ! -f ./images/ranking.jpg ]; then echo "module5 file 4" > ./images/ranking.jpg; fi
 
-echo "Listing content of bucket: s3://${19}..."
-aws s3 ls 
+echo "Uploading two objects to bucket ${19}..."
+aws s3 cp ./images/illinoistech.png s3://${19}/illinoistech.png
+aws s3 cp ./images/rohit.jpg s3://${19}/rohit.jpg
 
-# Upload ranking.jpg and elevate.webp to bucket ${20}
-echo "Uploading image: ./images/elevate.webp to s3://${20}..."
-aws s3 cp 
-echo "Uploaded image: ./images/elevate.webp to s3://${20}..."
+echo "Uploading two objects to bucket ${20}..."
+aws s3 cp ./images/elevate.webp s3://${20}/elevate.webp
+aws s3 cp ./images/ranking.jpg s3://${20}/ranking.jpg
 
-echo "Uploading image: ./images/ranking.jpg to s3://${20}..."
-aws s3 cp 
-echo "Uploaded image: ./images/ranking.jpg to s3://${20}..."
+echo "Listing content of bucket ${19}..."
+aws s3 ls s3://${19}
 
-echo "Listing content of bucket: s3://${20}..."
-aws s3 ls 
+echo "Listing content of bucket ${20}..."
+aws s3 ls s3://${20}
 
-# Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
-#https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/describe-load-balancers.html
-URL=
+echo "Retrieving ELB URL..."
+URL=$(aws elbv2 describe-load-balancers \
+  --load-balancer-arns $ELBARN \
+  --query 'LoadBalancers[0].DNSName' \
+  --output text)
 echo $URL
 
-# end of outer fi - based on arguments.txt content
 fi
--- itmo-444-544/module-05/destroy-env.sh
+++ itmo-444-544/module-05/destroy-env.sh
@@ -1,12 +1,135 @@
 #!/bin/bash
 ##############################################################################
-# Module-05
-# This assignment requires you to destroy the Cloud assets you created
-# Remember to set you default output to text in the aws config command
+# Module-05 Destroy Script
+# Deletes S3 objects/buckets, ASGs, EC2 instances, ELBs, target groups,
+# launch templates, and config.json.
 ##############################################################################
-ltconfigfile="./config.json"
 
 echo "Beginning destroy script for module-05 assessment..."
+
+TAGNAME="module5-tag"
+ltconfigfile="./config.json"
+
+echo "Deleting S3 objects and buckets..."
+BUCKETS=$(aws s3api list-buckets --query 'Buckets[*].Name' --output text)
+
+if [ "$BUCKETS" != "" ]
+then
+  for BUCKET in $BUCKETS
+  do
+    echo "Emptying bucket: $BUCKET"
+    aws s3 rm s3://$BUCKET --recursive
+
+    echo "Deleting bucket: $BUCKET"
+    aws s3api delete-bucket --bucket $BUCKET
+
+    echo "Waiting for bucket to be deleted: $BUCKET"
+    aws s3api wait bucket-not-exists --bucket $BUCKET
+  done
+else
+  echo "No S3 buckets found."
+fi
+
+echo "Finding Auto Scaling Groups..."
+ASGNAMES=$(aws autoscaling describe-auto-scaling-groups \
+  --query 'AutoScalingGroups[*].AutoScalingGroupName' \
+  --output text)
+
+if [ "$ASGNAMES" != "" ]
+then
+  for ASGNAME in $ASGNAMES
+  do
+    echo "Setting Auto Scaling Group to 0 capacity: $ASGNAME"
+    aws autoscaling update-auto-scaling-group \
+      --auto-scaling-group-name $ASGNAME \
+      --min-size 0 \
+      --max-size 0 \
+      --desired-capacity 0
+
+    echo "Deleting Auto Scaling Group: $ASGNAME"
+    aws autoscaling delete-auto-scaling-group \
+      --auto-scaling-group-name $ASGNAME \
+      --force-delete
+  done
+else
+  echo "No Auto Scaling Groups found."
+fi
+
+echo "Waiting for Auto Scaling cleanup..."
+sleep 90
+
+echo "Finding running or pending EC2 instances with tag $TAGNAME..."
+INSTANCEIDS=$(aws ec2 describe-instances \
+  --output text \
+  --query 'Reservations[*].Instances[*].InstanceId' \
+  --filters "Name=instance-state-name,Values=running,pending" "Name=tag:Name,Values=$TAGNAME")
+
+echo $INSTANCEIDS
+
+if [ "$INSTANCEIDS" != "" ]
+then
+  echo "Terminating remaining EC2 instances..."
+  aws ec2 terminate-instances --instance-ids $INSTANCEIDS
+
+  echo "Waiting for EC2 instances to terminate..."
+  aws ec2 wait instance-terminated --instance-ids $INSTANCEIDS
+else
+  echo "No running or pending EC2 instances found."
+fi
+
+echo "Finding Load Balancers..."
+ELBARNS=$(aws elbv2 describe-load-balancers \
+  --query 'LoadBalancers[*].LoadBalancerArn' \
+  --output text 2>/dev/null)
+
+if [ "$ELBARNS" != "" ]
+then
+  for ELBARN in $ELBARNS
+  do
+    echo "Deleting Load Balancer: $ELBARN"
+    aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
+  done
+
+  echo "Waiting for Load Balancers to be deleted..."
+  aws elbv2 wait load-balancers-deleted --load-balancer-arns $ELBARNS
+else
+  echo "No Load Balancers found."
+fi
+
+echo "Waiting before deleting target groups..."
+sleep 30
+
+echo "Finding Target Groups..."
+TARGETARNS=$(aws elbv2 describe-target-groups \
+  --query 'TargetGroups[*].TargetGroupArn' \
+  --output text 2>/dev/null)
+
+if [ "$TARGETARNS" != "" ]
+then
+  for TARGETARN in $TARGETARNS
+  do
+    echo "Deleting Target Group: $TARGETARN"
+    aws elbv2 delete-target-group --target-group-arn $TARGETARN
+  done
+else
+  echo "No Target Groups found."
+fi
+
+echo "Finding Launch Templates..."
+LAUNCHTEMPLATES=$(aws ec2 describe-launch-templates \
+  --query 'LaunchTemplates[*].LaunchTemplateName' \
+  --output text 2>/dev/null)
+
+if [ "$LAUNCHTEMPLATES" != "" ]
+then
+  for LAUNCHTEMPLATE in $LAUNCHTEMPLATES
+  do
+    echo "Deleting Launch Template: $LAUNCHTEMPLATE"
+    aws ec2 delete-launch-template --launch-template-name $LAUNCHTEMPLATE
+  done
+else
+  echo "No Launch Templates found."
+fi
 
 echo "Finding Launch template configuration file: $ltconfigfile..."
 if [ -a $ltconfigfile ]
@@ -16,185 +139,6 @@ then
   echo "Deleted Launch template configuration file: $ltconfigfile..."
 else
   echo "Launch template configuration file: $ltconfigfile doesn't exist, moving on..."
-# end of config.json delete
 fi
 
-# Collect Instance IDs
-INSTANCEIDS=$(aws ec2 describe-instances --output=text --query 'Reservations[*].Instances[*].InstanceId' --filter "Name=instance-state-name,Values=running")
-
-echo 'Finding autoscaling groups...'
-ASGNAMES=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[*].AutoScalingGroupName" --output text)
-if [ "$ASGNAMES" != "" ]
-  then
-    echo "Found AutoScalingGroups: $ASGNAMES..."
-    for ASGNAME in $ASGNAMES; do
-      echo "Processing Auto Scaling Group: $ASGNAME"
-
-      aws autoscaling update-auto-scaling-group \
-        --auto-scaling-group-name $ASGNAME \
-        --min-size
-
-      aws autoscaling update-auto-scaling-group \
-      --auto-scaling-group-name $ASGNAME \
-      --desired-capacity
-  
-     if [ "$INSTANCEIDS" != "" ]
-       then
-         # Trying a trick here to use the ec2 terminate-instance waiter to let the autoscaling group wind down the instances
-         echo "Waiting for all instances to be terminated..."
-         aws ec2 wait instance-terminated --instance-ids $INSTANCEIDS
-         echo "All instances terminated..."
-    else
-      echo "No instances to wait for termination..."
-      # end of internal if to check number of instances
-    fi
-     
-    done
-else
-  echo "No AutoScalingGroups Detected. Perhaps check if your create-env.sh script ran properly?"
-# End of ASG discovery and delete phase
-fi
-
-echo "Finding TARGETARN..."
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-target-groups.html
-TARGETARN=$(aws elbv2 describe-target-groups --query "TargetGroups[*].TargetGroupArn")
-if [ "$TARGETARN" != "" ]
-  then
-    echo "Found TargetARN: $TARGETARN..."
-  else
-    echo "Could not find any TargetARN. Perhaps check if the create-env.sh ran properly?"
-# End of TargetARN discovery
-fi
-
-if [ "$INSTANCEIDS" != "" ]
-  then
-    echo "\$INSTANCEIDS to be deregistered with the target group..."
-    # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/register-targets.html
-    # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
-    INSTANCEIDSARRAY=($INSTANCEIDS)
-    for INSTANCEID in ${INSTANCEIDSARRAY[@]};
-      do
-      echo "Deregistering target $INSTANCEID..."
-      aws elbv2 deregister-targets 
-      echo "Waiting for target $INSTANCEID to be deregistered..."
-      aws elbv2 wait target-deregistered 
-      done
-  else
-    echo 'There are no running or pending values in $INSTANCEIDS to wait for...'
-fi 
-
-echo "Looking up ELB ARN..."
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-load-balancers.html
-ELBARN=
-echo $ELBARN
-
-# Collect ListenerARN
-# https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/describe-listeners.html
- # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
-    ELBARNSARRAY=($ELBARN)
-    for ELB in ${ELBARNSARRAY[@]};
-      do
-        echo "Deleting Listener..."
-        LISTENERARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELB --query='Listeners[*].ListenerArn')
-        aws elbv2 delete-listener --listener-arn $LISTENERARN
-        echo "Listener deleted..."
-      done
-
-
-if [ "$TARGETARN" = "" ];
-  then  
-  echo "No Target Groups to delete..."
-else
-  echo "Deleting target group $TARGETARN..."
-  # Assignes the value of $EC2IDS and places each element (seperated by a space) into an array element
-  TARGETARNSARRAY=($TARGETARN)
-    for TGARN in ${TARGETARNSARRAY[@]};
-      do
-        # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/delete-target-group.html
-        aws elbv2 delete-target-group --target-group-arn $TGARN
-      done
-fi
-
-if [ "$ELBARN" = "" ];
-  then
-  echo "No ELBs to delete..."
-else
-  echo "Issuing Command to delete Load Balancer..."
-  # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/delete-load-balancer.html
-  aws elbv2 delete-load-balancer 
-  echo "Load Balancer delete command has been issued..."
-
-  echo "Waiting for ELB: $ELBARN to be deleted..."
-  # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/wait/load-balancers-deleted.html#examples
-  aws elbv2 wait load-balancers-deleted 
-  echo "ELB: $ELBARN deleted..." 
-fi
-
-echo 'Finding autoscaling groups for deletion...'
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/delete-auto-scaling-group.html
-ASGNAMES=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[*].AutoScalingGroupName")
-if [ "$ASGNAMES" = "" ];
-then
-  echo "No Autoscaling Groups found..."
-else
-  echo "Autoscaling Groups: $ASGNAMES found..."
-  ASGNAMESARRAY=($ASGNAMES)
-    for ASGNAME in ${ASGNAMESARRAY[@]};
-      do
-      echo "Deleting $ASGNAME..."
-      aws autoscaling delete-auto-scaling-group 
-      echo "Deleted $ASGNAME..."
-      done
-# End of if for checking on ASGs
-fi
-
-echo 'Finding lauch-templates...'
-LAUNCHTEMPLATEIDS=$(aws ec2 describe-launch-templates --query 'LaunchTemplates[].LaunchTemplateName' --output text)
-
-if [ "$LAUNCHTEMPLATEIDS" != "" ]
-  then
-    echo "Found launch-tempate: $LAUNCHTEMPLATEIDS..."
-    for LAUNCHTEMPLATEID in $LAUNCHTEMPLATEIDS; do
-      echo "Deleting launch-template: $LAUNCHTEMPID"
-      aws ec2 delete-launch-template --launch-template-name "$LAUNCHTEMPLATEID"
-    done
-else
-   echo "No launch-templates found. Perhpas you forget to run the create-env.sh script?"
-# end of if for launchtemplateids
-fi 
-
-# Query for bucket names, delete objects then buckets
-MYS3BUCKETS=$(aws s3api list-buckets )
-MYS3BUCKETS_ARRAY=($MYS3BUCKETS)
-
-#check for if list of buckets is non-zero (populated)
-if [ -n "$MYS3BUCKETS" ]
-  then 
-    echo "Looping through array of buckets to create array of objects..."
-    for j in "${MYS3BUCKETS_ARRAY[@]}"
-    do
-    MYKEYS=$(aws s3api list-objects-v2 )
-    MYKEYS_ARRAY=($MYKEYS)
-    echo "End of looping through array of buckets..."
-
-    echo "Looping through array of objects to delete them..."
-      for k in "${MYKEYS_ARRAY[@]}"
-      do
-      echo "Deleting object $k in bucket $j..."
-      aws s3api delete-object 
-      aws s3api wait object-not-exists 
-      echo "Deleted object $k in bucket $j..."
-      done
-    done
-
-    for l in "${MYS3BUCKETS_ARRAY[@]}"
-    do
-    echo "Deleting bucket $l..."
-    aws s3api delete-bucket 
-    aws s3api wait bucket-not-exists 
-    echo "Deleted bucket $l..."
-    done
-  else  
-    echo "There seems to be no buckets present -- did you run create-env.sh?"
-# end of s3 deletion block
-fi 
+echo "Finished destroying module-05 resources..."
